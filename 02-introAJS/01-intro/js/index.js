2dcb671105a087b585984e0d68bbbfe51dcadc24
Samael Bautista <samaelbautista18@gmail.com>
2022-01-24 21:27:38 -0600

    Intro a JS

---
+++ 02-introAJS/01-intro/js/index.js
@@ -0,0 +1,10 @@
+//Elementos para mostrar la salida de datos
+//Muestra la salida en consola
+console.log('Hola Mundo, desde un archivo externo')
+console.log(5+10)
+console.log(4==='4')
+
+//Muestra la salida en la pagina
+document.write('Hola Mundo, desde un archivo Externo15false')
+console.log(5+10)
+console.log(4==='4')
